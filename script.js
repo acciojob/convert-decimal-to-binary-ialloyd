15c580904add627fc4dca5936519771152635d7e
ialloyd <135848926+ialloyd@users.noreply.github.com>
2023-12-15 17:02:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function decimalToBinary(num) {
   // your code here
+	if (n == 0) {
+        return '0';
+    } else {
+        return n.toString(2)
+    }
+	
 }
-const num = parseInt(prompt("Enter a number."));
-alert(decimalToBinary(num));
+ const num = parseInt(prompt("Enter a number."));
+ alert(decimalToBinary(num));
